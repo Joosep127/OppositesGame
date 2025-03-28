767a88530437e7318f542e2605e5b20d1ccb0b3e
Joosep127 <48839946+Joosep127@users.noreply.github.com>
2025-03-28 20:29:22 +0200

    Added more complex movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -23,12 +23,29 @@ public class PlayerMovement : MonoBehaviour
 
 
     private Rigidbody2D rb;
-    public float moveSpeed = 5f;
+    public float acceleration = 1f;
+    public float maxSpeed = 10f;
+    public float maxFallSpeed = 10f;
+    public float deceleration = 0.6f;
+
+    public float accelerationAir;
+    public float decelerationAir;
+
+
+    public float jumpHangTimeThreshold;
+    public float jumpHangAccelerationMult;
+    public float jumpHangMaxSpeedMult;
+
+
     public float jumpForce = 10f;
     private float coyoteTime = 0.15f;
     public float coyoteTimeCounter = 0f;
     public bool isGrounded = false;
     private PlayerControls controls;
+    private PhysicsMaterial2D physicMaterial;
+
+    public float speedDif;
+    public float movement;
 
     void Start()
     {
@@ -47,6 +64,8 @@ public class PlayerMovement : MonoBehaviour
             Debug.LogError("GameObject is connected to an object that is not a player");
         }
 
+        physicMaterial = rb.sharedMaterial;
+
     }
 
     // Update is called once per frame
@@ -54,21 +73,66 @@ public class PlayerMovement : MonoBehaviour
     {
         if (isGrounded)// 
         {
-            coyoteTimeCounter = coyoteTime; // Reset timer when on the ground
+            coyoteTimeCounter = coyoteTime;
         }
         else
         {
-            coyoteTimeCounter -= Time.deltaTime; // Decrease timer when in the air
+            coyoteTimeCounter -= Time.deltaTime;
         }
 
-
-        float moveX = 0f;
+        int moveInput;
         if (Input.GetKey(controls.left))
-            moveX = -1f;
-        if (Input.GetKey(controls.right))
-            moveX = 1f;
+        {
+            moveInput = -1;
+        }
+        else if (Input.GetKey(controls.right))
+        {
+            moveInput = 1;
+        }
+        else
+        {
+            moveInput = 0;
+        }
 
-        rb.linearVelocity = new Vector2(moveX * moveSpeed, rb.linearVelocity.y);
+        float targetSpeed = moveInput * maxSpeed; // Credit to @DawnosaurDev in youtube
+        //We can reduce are control using Lerp() this smooths changes to are direction and speed
+        targetSpeed = Mathf.Lerp(rb.linearVelocity.x, targetSpeed, 1);
+
+        float accelRate;
+
+        //Gets an acceleration value based on if we are accelerating (includes turning) 
+        //or trying to decelerate (stop). As well as applying a multiplier if we're air borne.
+        if (isGrounded)
+            accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration : deceleration;
+        else
+            accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? acceleration * accelerationAir : deceleration * decelerationAir;
+
+        //Increase are acceleration and maxSpeed when at the apex of their jump, makes the jump feel a bit more bouncy, responsive and natural
+        
+
+        //We won't slow the player down if they are moving in their desired direction but at a greater speed than their maxSpeed
+        if (Mathf.Abs(rb.linearVelocity.x) > Mathf.Abs(targetSpeed) && Mathf.Sign(rb.linearVelocity.x) == Mathf.Sign(targetSpeed) && Mathf.Abs(targetSpeed) > 0.01f && isGrounded)
+        {
+            //Prevent any deceleration from happening, or in other words conserve are current momentum
+            //You could experiment with allowing for the player to slightly increae their speed whilst in this "state"
+            accelRate = 0;
+        }
+
+        if ((!isGrounded) && Mathf.Abs(rb.linearVelocity.y) < jumpHangTimeThreshold)
+        {
+            accelRate *= jumpHangAccelerationMult;
+            targetSpeed *= jumpHangMaxSpeedMult;
+        }
+
+        //Calculate difference between current velocity and desired velocity
+        float speedDif = targetSpeed - rb.linearVelocity.x;
+        //Calculate force along x-axis to apply to thr player
+
+        float movement = speedDif * accelRate; //
+
+        //Convert this to a vector and apply to rigidbody
+        rb.AddForce(movement * Vector2.right, 0);
+
 
         if (Input.GetKeyDown(controls.jump) && coyoteTimeCounter > 0)
         {
