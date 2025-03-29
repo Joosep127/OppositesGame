ad4a5457922fe703c5400f7e06e64234eb6b7ee6
Joosep127 <48839946+Joosep127@users.noreply.github.com>
2025-03-29 21:43:51 +0200

    Started buttons and doors working on player jumping on each others head ccapapility

---
+++ Assets/DoorScript.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Player Manager/PlayerMovement.cs
+++ Assets/Scripts/Player Manager/PlayerMovement.cs
@@ -112,6 +112,7 @@ public class PlayerMovement : MonoBehaviour
 
 
 
+
     }
 
     private float CalculateMovement()
